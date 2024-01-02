e2417630947fd4bfcfc3f7427477ee370dfc0d3d
mynameishomin <mynameishomin@gmail.com>
2024-01-02 22:07:58 +0900

    feat: 틱택토 게임 구현, 개선 필요

--- src/app/games/tictactoe/page.tsx
+++ src/app/games/tictactoe/page.tsx
@@ -13,6 +13,42 @@ export default () => {
         });
     };
 
+    const checkGameOver = (boardData: IGameCell[][], player: string) => {
+        const lineArray = [
+            // 가로 3줄
+            [boardData[0][0], boardData[0][1], boardData[0][2]],
+            [boardData[1][0], boardData[1][1], boardData[1][2]],
+            [boardData[2][0], boardData[2][1], boardData[2][2]],
+            // 세로 3줄
+            [boardData[0][0], boardData[1][0], boardData[2][0]],
+            [boardData[0][1], boardData[1][1], boardData[2][1]],
+            [boardData[0][2], boardData[1][2], boardData[2][2]],
+            // 대각선 2줄
+            [boardData[0][0], boardData[1][1], boardData[2][2]],
+            [boardData[0][2], boardData[1][1], boardData[2][0]],
+        ];
+        for (let i = 0; i < lineArray.length; i++) {
+            if (
+                lineArray[i].every(
+                    (item) => item.player.trim() === player.trim()
+                )
+            ) {
+                alert(`${player}의 승리`);
+                resetBoard();
+                break;
+            }
+        }
+
+        let cellArray = boardData.reduce(function (prev, next) {
+            return prev.concat(next);
+        });
+
+        if (cellArray.every((cell) => cell.value)) {
+            alert("무승부!");
+            resetBoard();
+        }
+    };
+
     const onClick = (y: number, x: number) => {
         if (boardData[y][x].value) return false;
 
@@ -21,8 +57,13 @@ export default () => {
             prev[y][x].value = true;
             prev[y][x].player = player;
             changeTurn();
+            checkGameOver([...prev], player);
             return [...prev];
         });
+    };
+
+    const resetBoard = () => {
+        setBoardData(createGridBoard(3, 3));
     };
 
     useEffect(() => {
