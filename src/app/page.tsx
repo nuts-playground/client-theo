c0a1031a1cd93594a9a4fe9e0599ae7c77249ab9
mynameishomin <mynameishomin@gmail.com>
2024-01-16 22:26:13 +0900

    feat: PlayerList 컴포넌트 분리

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,71 +2,11 @@
 import GameSection from "@/components/gameSection";
 import GameList from "@/components/gameList";
 import StatusSection from "@/components/statusSection";
-import {
-    Button,
-    ButtonGroup,
-    Listbox,
-    ListboxItem,
-    User,
-    Popover,
-    PopoverTrigger,
-    PopoverContent,
-} from "@nextui-org/react";
-import { useEffect, useState } from "react";
-import { io } from "socket.io-client";
-import { IPlayer } from "@/interface/interface";
-import { useAppSelector, useAppDispatch } from "./redux/hook";
-import { setPlayer, selectPlayer } from "./redux/playerSlice";
-import { setSocket } from "./redux/socketSlice";
+import { useAppSelector } from "./redux/hook";
+import { selectPlayer } from "./redux/playerSlice";
 import { JoinModal } from "@/components/joinModal";
 import { selectPlayers } from "./redux/playersSlice";
-
-const PlayerList = ({ players }: { players: IPlayer[] }) => {
-    return (
-        <section>
-            <h3 className="mb-1 pl-1 text-xl font-bold">Online players</h3>
-            <Listbox
-                className="mb-2 gap-0 bg-content1 w-[350px] max-w-full overflow-visible shadow-small rounded-medium"
-                aria-label="Actions"
-                emptyContent="There are no players currently online."
-            >
-                {players.map((player) => {
-                    return (
-                        <ListboxItem key={player.id}>
-                            <Popover showArrow placement="bottom">
-                                <PopoverTrigger>
-                                    <User
-                                        className="w-full justify-start"
-                                        name={`${player.name}`}
-                                        description={`Location: ${player.location}`}
-                                    />
-                                </PopoverTrigger>
-                                <PopoverContent className="p-1">
-                                    <ButtonGroup>
-                                        <Button
-                                            color="primary"
-                                            type="button"
-                                            size="sm"
-                                        >
-                                            Join the room
-                                        </Button>
-                                        <Button
-                                            color="primary"
-                                            type="button"
-                                            size="sm"
-                                        >
-                                            Send message
-                                        </Button>
-                                    </ButtonGroup>
-                                </PopoverContent>
-                            </Popover>
-                        </ListboxItem>
-                    );
-                })}
-            </Listbox>
-        </section>
-    );
-};
+import { PlayerList } from "@/components/playerList";
 
 export default function Home() {
     const player = useAppSelector(selectPlayer);
